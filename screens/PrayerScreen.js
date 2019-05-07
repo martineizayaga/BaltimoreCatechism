d717f244582fb28553de6d97f5d994445bc646db
martineizayaga <martineizayaga@gmail.com>
2019-05-07 19:39:13 -0400

    added ListItem to PrayerScreen

--- screens/PrayerScreen.js
+++ screens/PrayerScreen.js
@@ -1,5 +1,27 @@
 import React from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
+import { ListItem } from 'react-native-elements';
+
+const list = [
+  {
+    title: "The Lord's Prayer"
+  },
+  {
+    title: "The Angelical Salutation"
+  },
+  {
+    title: "The Apostle's Creed"
+  },
+  {
+    title: "The Confiteor"
+  },
+  {
+    title: "An Act of Faith"
+  },
+  {
+    title: "An Act of Hope"
+  }
+]
 
 export default class PrayerScreen extends React.Component {
   static navigationOptions = {
@@ -9,7 +31,14 @@ export default class PrayerScreen extends React.Component {
   render() {
     return (
       <ScrollView style={styles.container}>
-        
+        {
+          list.map((item, i) => (
+            <ListItem
+              key={i}
+              title={item.title}
+            />
+          ))
+        }
       </ScrollView>
     );
   }
