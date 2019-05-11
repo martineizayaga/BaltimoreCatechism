d3d24de5f258ea54c9173d7a4520d94f87502939
martineizayaga <martineizayaga@gmail.com>
2019-05-11 18:30:12 -0400

    readable folder organization

--- navigation/PrayerNavigator.js
+++
@@ -1,9 +0,0 @@
-import { createStackNavigator } from 'react-navigation'
-
-import ListOfPrayersScreen from '../screens/ListOfPrayersScreen';
-import OnePrayerScreen from '../prayers/OnePrayerScreen';
-
-export default PrayerNavigator = createStackNavigator({
-	ListOfPrayersScreen: { screen: ListOfPrayersScreen },
-	OnePrayerScreen: { screen: OnePrayerScreen }
-});
