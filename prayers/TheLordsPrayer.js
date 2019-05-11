ac7342144be562f015d6190d909c41bb64ca5eca
martineizayaga <martineizayaga@gmail.com>
2019-05-11 15:08:44 -0400

    added Our Father text

--- prayers/TheLordsPrayer.js
+++ prayers/TheLordsPrayer.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ListItem } from 'react-native-elements';
+import { ScrollView, StyleSheet, View } from 'react-native';
+import { ListItem, Text } from 'react-native-elements';
 
 export default class TheLordsPrayer extends React.Component {
-  static navigationOptions = {
-    title: "The Lord's Prayer",
-  };
 
-  render() {
-    return (
-      <ScrollView style={styles.container}>
-      </ScrollView>
-    );
-  }
+    render() {
+        return (
+            <ScrollView style={styles.container}>
+                <View>
+                    <Text h1>The Lord's Prayer</Text>
+                    <Text style={styles.prayer_text}>{`Our Father, who art in heaven, hallowed be Thy name: Thy kingdom come; Thy will be done on earth as it is in heaven. Give us this day our daily bread; and forgive us our trespasses as we forgive those who trespass against us: and lead us not into temptation, but deliver us from evil. Amen.`}</Text>
+                </View>
+            </ScrollView>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
-  },
+    prayer_text: {
+        margin: '0 auto'
+    },
+    prayer_container: {
+
+    },
+    container: {
+        flex: 1,
+        paddingTop: 15,
+        backgroundColor: '#fff',
+    },
 });
