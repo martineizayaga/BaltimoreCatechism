b6363827a2bb0c47448e14c11eb17249d79304b2
martineizayaga <martineizayaga@gmail.com>
2019-05-11 20:35:19 -0400

    basic spacing for lessons screen

--- screens/lessons/OneLessonScreen.js
+++ screens/lessons/OneLessonScreen.js
@@ -18,7 +18,7 @@ export default class OneLessonScreen extends React.Component {
             <ScrollView style={styles.container}>
                 {
                   lessons.map((item, i) => (
-                    <View key={item.number}>
+                    <View key={item.number} style={styles.question_container}>
                         <Text style={styles.question} h4>{ item.question }</Text>
                         <Text style={styles.answer}>{ item.answer }</Text>
                     </View>
@@ -34,6 +34,10 @@ const styles = StyleSheet.create({
         flex: 1,
         paddingTop: 15,
         backgroundColor: '#fff',
+    },
+    question_container: {
+        justifyContent: 'center',
+        marginHorizontal: 10
     },
     question: {
 
